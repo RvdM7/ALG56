c83ba08c67ba166fb181fa0ee2f1fcf742c08ffd
RvdM <rvdm1152@gmail.com>
2018-10-08 12:29:35 +0200

    looks commit
    made stuff look better

--- src/View/GridView.java
+++ src/View/GridView.java
@@ -3,10 +3,14 @@ package View;
 import Controller.Controller;
 import Dungeon.*;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Text;
+import javafx.stage.Screen;
 
 public class GridView implements View{
     private GridPane gp = new GridPane();
@@ -17,6 +21,14 @@ public class GridView implements View{
     }
 
     public void create(Controller con){
+        Rectangle2D boundaries = Screen.getPrimary().getVisualBounds();
+        ColumnConstraints ccHor = new ColumnConstraints(boundaries.getHeight());
+        RowConstraints ccVer = new RowConstraints(boundaries.getWidth());
+        ccHor.setPercentWidth(100.0/5);
+        ccVer.setPercentHeight(100.0/5);
+
+
+
         gp.setPadding(new Insets(10, 10, 10, 10));
         gp.setHgap(5);
         gp.setVgap(5);
@@ -26,6 +38,8 @@ public class GridView implements View{
 
         for (Room d : dg.getTest()) {
             gp.add(new Text("R"), d.getX(), d.getY());
+            gp.getColumnConstraints().addAll(ccHor);
+            gp.getRowConstraints().addAll(ccVer);
         }
     }
 
